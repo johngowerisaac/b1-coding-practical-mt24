c93d3915c93aece14ead61a67c76c9e780eac6a0
John Gower Isaac <john.gowerisaac@oriel.ox.ac.uk>
2024-10-23 15:33:27 +0100

    Improved control equation

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,11 +1,20 @@
-class PDController:
-    def __init__(self, Kp: float, Kd: float):
+class PIDController:
+    def __init__(self, Kp: float, Kd: float, Ki: float):
         self.Kp = Kp
         self.Kd = Kd
+        self.Ki = Ki
         self.previous_error = 0.0
+        self.integral = 0.0
 
-    def compute_control(self, reference: float, output:float) -> float:
+    def compute_control(self, reference: float, output: float) -> float:
         error = reference - output
-        control_action = self.Kp * error + self.Kd * (error - self.previous_error)
+        self.integral += error  # Accumulate error for integral term
+        derivative = error - self.previous_error
+        
+        # Compute the control action (PID control)
+        control_action = self.Kp * error + self.Kd * derivative + self.Ki * self.integral
+        
+        # Update previous error
         self.previous_error = error
+        
         return control_action
